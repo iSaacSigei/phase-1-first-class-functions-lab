5e22fd26ea89ab98f937bca399f4618275947da2
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-19 21:39:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+//return array of first two drivers
+const returnFirstTwoDrivers=function(drivers){
+    return [...drivers].slice(0,2)
+}
+
+//return array of last 2 drivers
+const returnLastTwoDrivers= function(drivers){
+    return [...drivers].slice(-2)
+}
+
+//selecting drivers
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+//fare multiplication
+const createFareMultiplier=function(fare){
+    return function(price){
+        return fare*price;
+    }
+}
+//fare multiplication to double and triple the fare
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3)
+
+//selection of drivers
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+  return returnFirstTwoDrivers(drivers);
+}
